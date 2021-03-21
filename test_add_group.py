9c8ebc879aeebdd75c0f473bb7f9c3f98ff68141
Артур <fazlyev.art@gmail.com>
2021-03-21 17:48:43 +0500

    test

--- test_add_group.py
+++ test_add_group.py
@@ -1,79 +1,41 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
 
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from group import Group
+from application import Application
 import unittest
 
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.app = Application()
 
     def test_app_dynamics_job(self):
-        wd = self.driver
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_group_page(wd)
-        self.create_group(wd, Group("test", "test", "test"))
-        self.returns_to_groups_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def returns_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, wd, group):
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("submit").click()
-
-    def open_group_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+        self.app.open_home_page()
+        self.app.login("admin", "secret")
+        self.app.open_group_page()
+        self.app.create_group(Group("test", "test", "test"))
+        self.app.returns_to_groups_page()
+        self.app.logout()
 
     def is_element_present(self, how, what):
         try:
-            self.driver.find_element(by=how, value=what)
+            self.app.wd.find_element(by=how, value=what)
         except NoSuchElementException as e:
             return False
         return True
 
     def is_alert_present(self):
         try:
-            self.driver.switch_to_alert()
+            self.app.wd.switch_to_alert()
         except NoAlertPresentException as e:
             return False
         return True
 
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.app.wd.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
@@ -84,7 +46,7 @@ class test_add_group(unittest.TestCase):
             self.accept_next_alert = True
 
     def tearDown(self):
-        self.assertEqual([], self.verificationErrors)
+        self.app.destroy()
 
 
 if __name__ == "__main__":
