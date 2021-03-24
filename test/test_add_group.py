c0496c7601766624a01cf71a4cf22a1c639e164f
Артур <fazlyev.art@gmail.com>
2021-03-24 23:13:51 +0500

    test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from sys import maxsize
+
 from model.group import Group
 
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("test", "test", "test"))
+    group = Group("test", "ds", "fsd")
+    app.group.create(group)
     app.group.returns_to_groups_page()
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
-    app.group.create(Group("test", "", ""))
+    app.group.create(Group("", "", ""))
     app.group.returns_to_groups_page()
