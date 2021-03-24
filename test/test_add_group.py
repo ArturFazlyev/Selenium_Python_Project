a56e86fa716a3a3284eaca2a786ab17d8e8d3880
Артур <fazlyev.art@gmail.com>
2021-03-24 21:55:31 +0500

    test

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,11 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group("test", "test", "test"))
     app.group.returns_to_groups_page()
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,8 +2,14 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(name='test3'))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
 
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     app.group.modify_first_group(Group(header='test3'))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
