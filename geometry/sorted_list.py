a150a2a1f92a660ec4898296f7a33781cbbfe2c9
Артур <fazlyev.art@gmail.com>
2021-03-25 22:45:37 +0500

    test

--- geometry/sorted_list.py
+++ geometry/sorted_list.py
@@ -2,6 +2,13 @@ from geometry.point import Point
 
 l1 = [Point(5, 2), Point(2, 3), Point(3, 6)]
 
-l2 = sorted(l1, key=lambda p: p.x)
+l = list(map(lambda i: Point(i, i * i), range(-5, 6)))
+n = list(filter(lambda p: p.x > 0, l))
+print(n)
+m = map(lambda r: r * r, range(0, 5))
+numbers = [10, 15, 21, 33, 42, 55]
+print(list(map(lambda x: x * 2 + 3, numbers)))
+print(list(m))
+print(l)
 
-print(l2)
+l2 = sorted(l1, key=lambda p: p.x)
